5cb61db69930e52e29bdfc36f332da5fbbbc3320
dinoocch <dino.occhialini@gmail.com>
2023-04-05 20:09:49 -0700

    Fix font

--- files/wezterm.lua
+++ files/wezterm.lua
@@ -1,5 +1,6 @@
-local config = require("wezterm").config_builder()
-
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+config.font = wezterm.font("Comic Code Ligatures")
 config.color_scheme = "Catppuccin Frappe"
 
 return config

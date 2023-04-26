1a36db30cd054e8145d2834b55318af7d95a4a24
dinoocch <dino.occhialini@gmail.com>
2023-04-26 16:58:32 -0700

    Add pyright config

--- files/nvim/lua/languages/python.lua
+++ files/nvim/lua/languages/python.lua
@@ -1,22 +1,21 @@
 return {
-
-    -- Python is included by default in treesitter
-    -- {
-    --   "nvim-treesitter/nvim-treesitter",
-    --   opts = function(_, opts)
-    --     if type(opts.ensure_installed) == "table" then
-    --       vim.list_extend(opts.ensure_installed, { "python" })
-    --     end
-    --   end,
-    -- },
-
     -- correctly setup lspconfig
     {
         "neovim/nvim-lspconfig",
         opts = {
             -- make sure mason installs the server
             servers = {
-                pyright = {},
+                pyright = {
+                    settings = {
+                        python = {
+                            analysis = {
+                                autoSearchPaths = true,
+                                useLibraryCodeForTypes = true,
+                                diagnosticMode = "openFilesOnly",
+                            },
+                        },
+                    },
+                },
             },
         },
     },

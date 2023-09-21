f2fbf219f31b155432895b648ee58cbb08a40c66
dinoocch <dino.occhialini@gmail.com>
2023-09-20 18:09:40 -0700

    Add pulse

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -43,8 +43,10 @@
     pipewire = {
       enable = true;
       package = pkgs-unstable.pipewire;
+      audio.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
+      pulse.enable = true;
       jack.enable = true;
       wireplumber.enable = true;
     };

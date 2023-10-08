564575b290080c423190480997964d4ac230471d
dinoocch <dino.occhialini@gmail.com>
2023-10-08 16:44:01 -0700

    savepoint

--- home/desktop/i3/default.nix
+++ home/desktop/i3/default.nix
@@ -1,6 +1,7 @@
 { pkgs, pkgs-unstable, ... }: {
   home.packages = with pkgs; [
     pkgs-unstable.i3status-rust
+    (pkgs.callPackage ../../../packages/i3-autolayout.nix {})
   ];
 
   xdg.configFile."i3/config".source = ./config;

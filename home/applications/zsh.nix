9d6e24aacab4116574e0f06da1c6e39e4b919ac2
dinoocch <dino.occhialini@gmail.com>
2024-08-14 14:21:04 -0700

    Mac zprofile updates

--- home/applications/zsh.nix
+++ home/applications/zsh.nix
@@ -19,10 +19,17 @@
     };
     defaultKeymap = "viins";
     initExtra = ''
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
       source "${pkgs-unstable.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh"
       zsh-defer source "${pkgs.skim}/share/skim/key-bindings.zsh"
       zsh-defer source "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh"
       zsh-defer source "${pkgs.grc}/etc/grc.zsh"
+      path=('/Users/docchial/.cargo/bin' $path)
+      export PATH
+      export VOLTA_HOME="$HOME/.volta"
+      export PATH="$VOLTA_HOME/bin:$PATH"
     '';
   };
 
